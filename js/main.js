32f608acab18de3a845bc13fc9363b9ef1aee64f
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-24 14:57:18 +0200

    show numbers on screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const simonContainerNumbers = document.querySelectorAll(".simon-numbers");
 const generateNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
@@ -5,4 +6,5 @@ const generateNumber = (min, max) => {
 let simonNumbers = [];
 for (let i = 0; i < 5; i++) {
   simonNumbers[i] = generateNumber(0, 20);
+  simonContainerNumbers[i].innerHTML = simonNumbers[i];
 }
