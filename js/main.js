5c81af399f7e1ff03c484097bad573cd23c4c86e
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-24 15:21:50 +0200

    fix input numbers

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 const simonContainerNumbers = document.querySelectorAll(".simon-numbers");
+const secondsRemainingCounter = document.getElementById("seconds-remaining");
+const secondsToDisappear = 5;
+let simonNumbers = [];
+let secondsRemaining = secondsToDisappear;
+secondsRemainingCounter.innerHTML = secondsRemaining;
+
 const generateNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-let simonNumbers = [];
 for (let i = 0; i < 5; i++) {
   simonNumbers[i] = generateNumber(0, 20);
-  simonContainerNumbers[i].innerHTML = simonNumbers[i];
+  simonContainerNumbers[i].value = simonNumbers[i];
 }
+
+setInterval(() => {
+  for (let i = 0; i < 5; i++) {
+    simonContainerNumbers[i].value = "";
+  }
+}, secondsToDisappear * 1000);
+
+const countdown = () => {
+  if (secondsRemaining <= 1) {
+    clearInterval(countdownInterval);
+  }
+  secondsRemaining--;
+  secondsRemainingCounter.innerHTML = secondsRemaining;
+};
+
+const countdownInterval = setInterval(countdown, 1000);
